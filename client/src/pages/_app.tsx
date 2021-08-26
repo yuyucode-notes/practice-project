ec7981151cdf2202b7595e41535a7526483d953c
yuyucode1125 <1440771511@qq.com>
2021-08-26 23:06:33 +0800

    feat(client): 首页更改

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app'
+import {createContext, useState} from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <Component {...pageProps} />
+  )
+
+
 }
 export default MyApp
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type {NextPage} from 'next'
+import styles from '../styles/Home.module.css'
+import GoodsControlCheckBoxGroup from "@/componets/"
+
+
+const Home: NextPage = () => {
+
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+
+export default Home
